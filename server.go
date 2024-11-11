b2f4a23e1457b7e58ef90ecb44585ff516be7d17
fujiwara <fujiwara.shunichiro@gmail.com>
2024-11-11 17:11:46 +0900

    use trigram, etc

--- server.go
+++ server.go
@@ -7,29 +7,39 @@ import (
 	"log/slog"
 	"net/http"
 	"net/url"
+	"os"
 	"sort"
 	"strings"
+	"unicode/utf8"
 
 	"github.com/fujiwara/ridge"
 	"github.com/jmoiron/sqlx"
 	"github.com/labstack/echo/v4"
 	"github.com/samber/lo"
+	sloghttp "github.com/samber/slog-http"
 )
 
+var logger = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
 func runServer(ctx context.Context) error {
+
 	e := echo.New()
 	e.Renderer = newTemplates()
 	e.GET("/", rootHandler)
 	e.GET("/static/*", staticHandler)
-	e.GET("/:channel/", channelHandler)
-	e.GET("/:channel/:log_date", contentsHandler)
+	e.GET("/log/:channel/", channelHandler)
+	e.GET("/log/:channel/:log_date", contentsHandler)
 	e.GET("/search", searchHandler)
-	ridge.RunWithContext(ctx, config.Server.Addr, "/", e)
+
+	// add logger middleware
+	handler := sloghttp.Recovery(e)
+	handler = sloghttp.New(logger)(handler)
+	ridge.RunWithContext(ctx, config.Server.Addr, "/", handler)
 	return nil
 }
 
 func errorResponse(c echo.Context, status int, err error) error {
-	slog.Error(err.Error())
+	logger.Error(err.Error())
 	return c.String(status, http.StatusText(status))
 }
 
@@ -40,9 +50,7 @@ func rootHandler(c echo.Context) error {
 		return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to open DB: %w", err))
 	}
 	defer db.Close()
-	if err != nil {
-		return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to query DB: %w", err))
-	}
+
 	channels, err := listChannels(ctx, db)
 	if err != nil {
 		return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to query DB: %w", err))
@@ -102,7 +110,7 @@ func channelHandler(c echo.Context) error {
 }
 
 type TiarraLog struct {
-	ID      int    `db:"id"`
+	ID      int    `db:"rowid"`
 	Channel string `db:"channel"`
 	LogDate string `db:"log_date"`
 	Content string `db:"content"`
@@ -110,8 +118,11 @@ type TiarraLog struct {
 
 func contentsHandler(c echo.Context) error {
 	ctx := c.Request().Context()
-	channel, _ := url.PathUnescape(c.Param("channel"))
-	logDate := c.Param("log_date")
+	channel, err := url.PathUnescape(c.Param("channel"))
+	if err != nil {
+		return errorResponse(c, http.StatusBadRequest, fmt.Errorf("failed to unescape channel: %w", err))
+	}
+	logDate := strings.TrimSuffix(c.Param("log_date"), ".txt")
 
 	db, err := openDB()
 	if err != nil {
@@ -119,9 +130,13 @@ func contentsHandler(c echo.Context) error {
 	}
 	defer db.Close()
 	logs := []TiarraLog{}
-	if err := db.SelectContext(ctx, &logs, "SELECT content FROM tiarra WHERE channel = ? AND log_date = ? ORDER BY id LIMIT 1", channel, logDate); err != nil {
+	if err := db.SelectContext(ctx, &logs, "SELECT content FROM tiarra WHERE channel = ? AND log_date = ?", channel, logDate); err != nil {
 		return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to query DB: %w", err))
 	}
+	if len(logs) == 0 {
+		return c.String(http.StatusNotFound, "Not Found")
+	}
+
 	return c.Render(http.StatusOK, "contents.html", map[string]interface{}{
 		"Title":    fmt.Sprintf("IRC Log Viewer / %s / %s", channel, logDate),
 		"Channel":  channel,
@@ -132,6 +147,16 @@ func contentsHandler(c echo.Context) error {
 	})
 }
 
+func quoteMatch(s string) string {
+	ws := []string{}
+	for _, w := range strings.Split(s, " ") {
+		w = strings.ReplaceAll(w, `"`, ``)
+		w = strings.Trim(w, ` `)
+		ws = append(ws, `"`+w+`"`)
+	}
+	return strings.Join(ws, " ")
+}
+
 func searchHandler(c echo.Context) error {
 	ctx := c.Request().Context()
 	db, err := openDB()
@@ -140,61 +165,77 @@ func searchHandler(c echo.Context) error {
 	}
 	defer db.Close()
 	logs := []TiarraLog{}
-	matches := []string{}
 	q := c.QueryParam("search")
-	if len(q) < 2 {
-		q = q + " "
-	}
 	firstWord := strings.ToLower(strings.Split(q, " ")[0])
-	channel := url.PathEscape(c.QueryParam("channel"))
-	ng := createNgrams(q, 2)
-	for _, n := range ng {
-		if strings.Contains(n, `"`) {
-			continue
-		}
-		matches = append(matches, `"`+n+`"`)
+	channel, err := url.PathUnescape(c.QueryParam("channel"))
+	if err != nil {
+		return errorResponse(c, http.StatusBadRequest, fmt.Errorf("failed to unescape channel: %w", err))
 	}
+	match := quoteMatch(q)
 	if channel == "" {
-		if err := db.SelectContext(ctx, &logs,
-			`SELECT DISTINCT tiarra.* FROM tiarra JOIN tiarra_fts USING(id)
-			WHERE tiarra_fts MATCH ? ORDER BY rank LIMIT 100`,
-			strings.Join(matches, " "),
-		); err != nil {
+		query := `SELECT DISTINCT tiarra.* FROM tiarra JOIN tiarra_fts ON (tiarra.rowid=tiarra_fts.rowid)
+				WHERE tiarra_fts MATCH ? ORDER BY rank LIMIT 100`
+		logger.Info("searching", "query", query, "match", match)
+		if err := db.SelectContext(ctx, &logs, query, match); err != nil {
 			return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to query DB: %w", err))
 		}
 	} else {
+		logger.Info("searching", "channel", channel, "match", match)
 		if err := db.SelectContext(ctx, &logs,
-			`SELECT DISTINCT tiarra.* FROM tiarra JOIN tiarra_fts USING(id)
+			`SELECT DISTINCT tiarra.* FROM tiarra JOIN tiarra_fts ON (tiarra.rowid=tiarra_fts.rowid)
 			WHERE tiarra_fts MATCH ? AND channel = ? ORDER BY rank LIMIT 100`,
-			strings.Join(matches, " "), channel,
+			match, channel,
 		); err != nil {
 			return errorResponse(c, http.StatusInternalServerError, fmt.Errorf("failed to query DB: %w", err))
 		}
 	}
+	logger.Info("search result", "logs", len(logs))
 	filteredLogs := []TiarraLog{}
 	channels := []string{}
 	for _, log := range logs {
 		var content string
+		matched := 0
 		for _, line := range strings.Split(log.Content, "\n") {
-			if strings.Contains(strings.ToLower(line), firstWord) {
+			msg, _ := parseLogLine(line)
+			if strings.Contains(strings.ToLower(msg), firstWord) {
 				content += line + "\n"
+				logger.Debug("matched", "msg", msg)
+				matched++
+			}
+			if matched > 10 {
+				break
 			}
 		}
 		if content != "" {
 			log.Content = content
-			filteredLogs = append(filteredLogs, log)
-			channels = append(channels, log.Channel)
+		} else {
+			log.Content = prefix(log.Content, 256)
 		}
+		filteredLogs = append(filteredLogs, log)
+		channels = append(channels, log.Channel)
 	}
 	channels = lo.Uniq(channels)
 	sort.Strings(channels)
 
-	return c.Render(http.StatusOK, "search.html", map[string]interface{}{
+	v := map[string]interface{}{
 		"Title":    fmt.Sprintf("IRC Log Viewer / Search: %s", c.QueryParam("search")),
 		"Logs":     filteredLogs,
 		"Query":    c.QueryParam("search"),
 		"Channels": channels,
-	})
+	}
+	if utf8.RuneCountInString(q) <= 2 {
+		v["Warning"] = true
+	}
+	return c.Render(http.StatusOK, "search.html", v)
+}
+
+func prefix(s string, n int) string {
+	if utf8.RuneCountInString(s) > n {
+		runes := []rune(s)
+		return string(runes[:n])
+	} else {
+		return s
+	}
 }
 
 //go:embed static/*
